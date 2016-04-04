33fb373394a2ee1f347e7e38fcdbe21d5665a998
jzelezny <jiri.zelezny@blackboard.com>
2016-04-04 15:26:54 +0200

    Arquillian tests with embedded Wildfly 10 in /target..ufff

--- src/test/java/muni/fi/dp/jz/jbatch/batchapi/BatchExecutionBeanTest.java
+++ src/test/java/muni/fi/dp/jz/jbatch/batchapi/BatchExecutionBeanTest.java
@@ -15,43 +15,56 @@
  */
 package muni.fi.dp.jz.jbatch.batchapi;
 
-import java.util.List;
-import java.util.Properties;
-import java.util.Set;
-import javax.batch.runtime.JobExecution;
+import javax.annotation.Resource;
 import javax.batch.runtime.JobInstance;
-import javax.batch.runtime.StepExecution;
-import javax.ejb.embeddable.EJBContainer;
+import javax.ejb.EJB;
+import muni.fi.dp.jz.jbatch.exception.BatchExecutionException;
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
 
 /**
  *
  * @author Zorz
  */
+@RunWith(Arquillian.class)
 public class BatchExecutionBeanTest {
-    
-    public BatchExecutionBeanTest() {
-    }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
+    }
+
+    @EJB    
+    private BatchExecutionBean batchExecutor;
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        JavaArchive jar = ShrinkWrap.create(JavaArchive.class);
+        jar.addPackage("muni.fi.dp.jz.batchapi");
+        jar.addClass(BatchExecutionBean.class);
+        jar.addClass(BatchExecutionException.class);
+        return jar;
     }
 
     /**
@@ -60,15 +73,9 @@ public class BatchExecutionBeanTest {
     @Test
     public void testSubmitJob() throws Exception {
         System.out.println("submitJob");
-        String jobName = "";
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        BatchExecutionBean instance = (BatchExecutionBean)container.getContext().lookup("java:global/classes/BatchExecutionBean");
-        long expResult = 0L;
-        long result = instance.submitJob(jobName);
-        assertEquals(expResult, result);
-        container.close();
-//         TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        long expected = 1;
+        String jobName = "abc";
+       Assert.assertEquals(expected, batchExecutor.submitJob(jobName));        
     }
 
     /**
@@ -77,13 +84,7 @@ public class BatchExecutionBeanTest {
     @Test
     public void testGetJobExecution() throws Exception {
         System.out.println("getJobExecution");
-        Long executionId = null;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        BatchExecutionBean instance = (BatchExecutionBean)container.getContext().lookup("java:global/classes/BatchExecutionBean");
-        JobExecution expResult = null;
-        JobExecution result = instance.getJobExecution(executionId);
-        assertEquals(expResult, result);
-        container.close();
+        
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -94,13 +95,7 @@ public class BatchExecutionBeanTest {
     @Test
     public void testRestartJob() throws Exception {
         System.out.println("restartJob");
-        long executionId = 0L;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        BatchExecutionBean instance = (BatchExecutionBean)container.getContext().lookup("java:global/classes/BatchExecutionBean");
-        long expResult = 0L;
-        long result = instance.restartJob(executionId);
-        assertEquals(expResult, result);
-        container.close();
+        
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -111,12 +106,7 @@ public class BatchExecutionBeanTest {
     @Test
     public void testGetJobNames() throws Exception {
         System.out.println("getJobNames");
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        BatchExecutionBean instance = (BatchExecutionBean)container.getContext().lookup("java:global/classes/BatchExecutionBean");
-        Set<String> expResult = null;
-        Set<String> result = instance.getJobNames();
-        assertEquals(expResult, result);
-        container.close();
+        
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -127,13 +117,7 @@ public class BatchExecutionBeanTest {
     @Test
     public void testGetJobInstanceCount() throws Exception {
         System.out.println("getJobInstanceCount");
-        String jobName = "";
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        BatchExecutionBean instance = (BatchExecutionBean)container.getContext().lookup("java:global/classes/BatchExecutionBean");
-        int expResult = 0;
-        int result = instance.getJobInstanceCount(jobName);
-        assertEquals(expResult, result);
-        container.close();
+        
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -144,13 +128,7 @@ public class BatchExecutionBeanTest {
     @Test
     public void testGetRunningExecutions() throws Exception {
         System.out.println("getRunningExecutions");
-        String jobName = "";
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        BatchExecutionBean instance = (BatchExecutionBean)container.getContext().lookup("java:global/classes/BatchExecutionBean");
-        List<Long> expResult = null;
-        List<Long> result = instance.getRunningExecutions(jobName);
-        assertEquals(expResult, result);
-        container.close();
+       
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -162,12 +140,7 @@ public class BatchExecutionBeanTest {
     public void testGetJobInstances_String() throws Exception {
         System.out.println("getJobInstances");
         String jobName = "";
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        BatchExecutionBean instance = (BatchExecutionBean)container.getContext().lookup("java:global/classes/BatchExecutionBean");
-        List<JobInstance> expResult = null;
-        List<JobInstance> result = instance.getJobInstances(jobName);
-        assertEquals(expResult, result);
-        container.close();
+       
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -181,12 +154,7 @@ public class BatchExecutionBeanTest {
         String jobName = "";
         int start = 0;
         int count = 0;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        BatchExecutionBean instance = (BatchExecutionBean)container.getContext().lookup("java:global/classes/BatchExecutionBean");
-        List<JobInstance> expResult = null;
-        List<JobInstance> result = instance.getJobInstances(jobName, start, count);
-        assertEquals(expResult, result);
-        container.close();
+       
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -198,12 +166,7 @@ public class BatchExecutionBeanTest {
     public void testGetJobExecutions() throws Exception {
         System.out.println("getJobExecutions");
         JobInstance instance_2 = null;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        BatchExecutionBean instance = (BatchExecutionBean)container.getContext().lookup("java:global/classes/BatchExecutionBean");
-        List<JobExecution> expResult = null;
-        List<JobExecution> result = instance.getJobExecutions(instance_2);
-        assertEquals(expResult, result);
-        container.close();
+        
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -215,12 +178,7 @@ public class BatchExecutionBeanTest {
     public void testGetStepExecutions() throws Exception {
         System.out.println("getStepExecutions");
         long jobExecutionId = 0L;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        BatchExecutionBean instance = (BatchExecutionBean)container.getContext().lookup("java:global/classes/BatchExecutionBean");
-        List<StepExecution> expResult = null;
-        List<StepExecution> result = instance.getStepExecutions(jobExecutionId);
-        assertEquals(expResult, result);
-        container.close();
+        
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -232,12 +190,7 @@ public class BatchExecutionBeanTest {
     public void testGetJobInstance() throws Exception {
         System.out.println("getJobInstance");
         long executionId = 0L;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        BatchExecutionBean instance = (BatchExecutionBean)container.getContext().lookup("java:global/classes/BatchExecutionBean");
-        JobInstance expResult = null;
-        JobInstance result = instance.getJobInstance(executionId);
-        assertEquals(expResult, result);
-        container.close();
+       
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -249,10 +202,7 @@ public class BatchExecutionBeanTest {
     public void testStop() throws Exception {
         System.out.println("stop");
         long executionId = 0L;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        BatchExecutionBean instance = (BatchExecutionBean)container.getContext().lookup("java:global/classes/BatchExecutionBean");
-        instance.stop(executionId);
-        container.close();
+       
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -264,10 +214,7 @@ public class BatchExecutionBeanTest {
     public void testAbandon() throws Exception {
         System.out.println("abandon");
         long executionId = 0L;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        BatchExecutionBean instance = (BatchExecutionBean)container.getContext().lookup("java:global/classes/BatchExecutionBean");
-        instance.abandon(executionId);
-        container.close();
+       
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
@@ -279,14 +226,9 @@ public class BatchExecutionBeanTest {
     public void testGetParameters() throws Exception {
         System.out.println("getParameters");
         long l = 0L;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        BatchExecutionBean instance = (BatchExecutionBean)container.getContext().lookup("java:global/classes/BatchExecutionBean");
-        Properties expResult = null;
-        Properties result = instance.getParameters(l);
-        assertEquals(expResult, result);
-        container.close();
+       
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-    
+
 }
